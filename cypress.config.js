11bc7d5127478761ab4ce0680e34dbe8f8584f98
parimalgdekate <parimalgdekate@gmail.com>
2023-06-03 14:23:04 +0530

    changed the report type to single page report for easy sharing

--- cypress.config.js
+++ cypress.config.js
@@ -2,12 +2,17 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   reporter: 'cypress-mochawesome-reporter',
+  video: false,
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'Cypress - Test Result Report',
+    embeddedScreenshots: true,
+    inlineAssets: true, //Adds the asserts inline
+  },
   e2e: {
     specPattern: 'cypress/e2e/*.js',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
       require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
-  
 });
