faf6a8daaa0e8835da3016207237eec80f379802
parimalgdekate <parimalgdekate@gmail.com>
2023-06-03 13:58:13 +0530

    cypress mochawesome reporter - Checked with seperate repo and then implemented in working repo MM

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  e2e: {
+    specPattern: 'cypress/e2e/*.js',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
+  },
+  
+});
